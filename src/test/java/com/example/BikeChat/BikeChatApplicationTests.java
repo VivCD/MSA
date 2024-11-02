e9b524faa868ab0ecab9d0c28a97379d2472dd97
alexa <alexabianca091@gmail.com>
2024-11-02 11:55:53 +0200

    SpringBoot init

---
+++ src/test/java/com/example/BikeChat/BikeChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BikeChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
